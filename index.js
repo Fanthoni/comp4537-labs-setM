88ffe2979dd966f6443c5d4ecdb23d056b07d75c
ferrelanthoni <ferrelanthoni@gmail.com>
2022-09-28 21:53:42 -0700

    lab4 commit

--- index.js
+++ index.js
@@ -1,83 +1,95 @@
-const {readFile, writeFile} = require('fs')
-const util = require('util')
-
-const express = require("express")
+const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require("body-parser")
+const {Schema} = mongoose
+
+const unicornSchema = new Schema({
+  "name": String, // String is shorthand for {type: String}
+  "weight": Number,
+  "loves": [String],
+  "gender": {
+    enum: ["f", "m"]
+  },
+  "vampires": Number,
+  "dob": Date
+});
+const unicornModel = mongoose.model('unicorns', unicornSchema)
+
+
 const app = express()
-// var { unicornsJSON } = require('./data.js')
-var unicornsJSON = []
-
-const readFileAsync = util.promisify(readFile)
-const writeFileAsync = util.promisify(writeFile)
-
 const port = 5050
 
-// Middlewares
 app.use(bodyParser.json())
 app.use(express.json())
 
-app.get('/api/v1/unicorns', (req, res) => {
-    res.send(unicornsJSON)
-})
-  
-app.post('/api/v1/unicorn', (req, res) => {
-    unicornsJSON.push(req.body)
-    console.log(unicornsJSON)
-    //update the file
-    writeFileAsync('./data.json', JSON.stringify(unicornsJSON), "utf-8")
-      .then(() => { })
-      .catch((err) => { console.log(err); })
-    res.end()
-})
-  
-app.get('/api/v1/unicorn/:id', (req, res) => {
-    var found = false
-    for (i = 0; i < unicornsJSON.length; i++) {
-      if (unicornsJSON[i]._id == req.params.id) {
-        found = true
-        break
-      }
-    }
-  
-    if (found) { res.json(unicornsJSON[i]); return }
-    res.json({ msg: "not found" })
-})
-  
-app.patch('/api/v1/unicorn/:id', (req, res) => {
-  const {id} = req.params
-    for (let i = 0; i < unicornsJSON.length; i++) {
-      if (unicornsJSON[i]._id === id) {
-        unicornsJSON[i] = req.body
-      }
-    }
-    writeFileAsync('./data.json', JSON.stringify(unicornsJSON), "utf-8")
-      .then(() => { })
-      .catch((err) => { console.log(err); })
-    res.send("Updated successfully!")
-})
-  
-app.delete('/api/v1/unicorn/:id', (req, res) => {
-    unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
-    writeFileAsync('./data.json', JSON.stringify(unicornsJSON), "utf-8")
-      .then(() => { })
-      .catch((err) => { console.log(err); })
-    res.send("Deleted successfully?")
+// app.get('/api/v2/unicorns')           // - get all the unicorns
+// app.post('/api/v2/unicorn')          // - create a new unicorn
+// app.get('/api/v2/unicorn/:id')       // - get a unicorn
+// app.patch('/api/v2/unicorn/:id')     // - update a unicorn
+// app.delete('/api/v2/unicorn/:id')       // - delete unicorn
+
+app.get("/api/v2/unicorns", async (req, res) => {
+  const unicorns = unicornModel.find({})
+    .then((docs) => {
+      res.json(docs).end()
+    })
+    .catch((err) => {
+      console.log(`Error encountered when getting all unicorns ${err}`)
+      res.end("Error")
+    });
 })
 
-app.listen(process.env.PORT || port, async (err) => {
-  if (err) {
-    console.error(err);
-  }
-  try {
-    unicornsJSON = await readFileAsync('./data.json', 'utf-8')
-    if (!unicornsJSON) {
-      console.log("Could not read the file");
-      return
-    }
-    unicornsJSON = JSON.parse(unicornsJSON)
-  } catch (error) {
-    console.log(error);
-  }
-
-  console.log(`Example app listening on port ${port}`)
+app.get("/api/v2/unicorn/:id", async (req, res) => {
+  const {id} = req.params;
+  const unicron = unicornModel.find({_id: mongoose.Types.ObjectId(`${id}`)})
+    .then((doc) => {
+      console.log(doc);
+      res.json(doc).end()
+    })
+    .catch((err) => {
+      console.log(`Error encountered when getting unicorn ${err}`)
+      res.end("Error")
+    })
 })
+
+app.post("/api/v2/unicorn", async (req, res) => {
+  const newUnicorn = req.body;
+  unicornModel.create(newUnicorn, (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+  res.json(newUnicorn)
+})
+
+app.patch("/api/v2/unicorn/:id", async (req, res) => {
+  const {__id, ...newValues} =  req.body;
+  unicornModel.updateOne({_id: mongoose.Types.ObjectId(req.params.id)}, 
+    newValues, 
+    (error, res) => {
+      if (error) console.error(error)
+      console.log(res)
+    })
+  res.send("Updated successfuly!")
+})
+
+app.delete("/api/v2/unicorn/:id", async (req, res) => {
+  unicornModel.deleteOne({_id: mongoose.Types.ObjectId(req.params.id)}, 
+    function (err, result) {
+      if (err) {
+        console.error(err)
+      }
+      console.log(result);
+    })
+  res.send(`Unicorn ${req.params.id} deleted successfully`)
+})
+
+app.listen(port, async () => {
+    try {
+      await mongoose.connect('mongodb://localhost:27017/test')
+    } catch (error) {
+      console.log('db connection error', error);
+    }
+    console.log(`Example app listening on port ${port}`)
+  }
+)
