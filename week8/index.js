4aaa63db9fc5d0a37994aa04581ef419302cf1b7
ferrelanthoni <ferrelanthoni@gmail.com>
2022-10-27 22:30:00 -0700

    week 8 initial push

---
+++ week8/index.js
@@ -0,0 +1,374 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const axios = require('axios')
+const {Schema} = mongoose
+const getThreeCharDigit = require("./utility");
+
+const app = express()
+const port = 5050
+
+// Middlewares
+app.use(express.json())
+
+var pokemonSchema;
+var pokemonModel;
+
+// app.get('/api/v1/pokemons?count=2&after=10')     // - get all the pokemons after the 10th. List only Two.
+// app.post('/api/v1/pokemon')                      // - create a new pokemon
+// app.get('/api/v1/pokemon/:id')                   // - get a pokemon
+// app.get('/api/v1/pokemonImage/:id')               // - get a pokemon Image URL
+// app.patch('/api/v1/pokemon/:id')                 // - update a pokemon
+// app.delete('/api/v1/pokemon/:id')                // - delete a  pokemon
+
+app.listen(process.env.PORT || port, async (err) => {
+  if (err) {
+    console.log(err)
+  }
+    try {
+      await mongoose.connect('mongodb+srv://testUser:COMP4537_password@cluster0.vtxvo7p.mongodb.net/?retryWrites=true&w=majority')
+      
+      var pokemonTypes;
+      await axios.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json")
+        .then((res) => {
+          pokemonTypes = res.data.map(type => {
+            return type.english;
+          });
+        })
+        .catch((err) => {
+          console.log('err', err)
+        })
+      pokemonSchema = new Schema({
+        "id": {type: String, unique: true},
+        "name": {
+          "english": {type: String, maxlength: 20},
+          "japanese": {type: String},
+          "chinese": {type: String},
+          "french": {type: String},
+        },
+        "type": {
+          type: [String], enum: pokemonTypes
+        },
+        "base": {
+          "HP": {type: Number, required: true},
+          "Attack": {type: Number, required: true},
+          "Defense": {type: Number, required: true},
+          "Speed": {type: Number, required: true},
+          "Speed Attack": {type: Number, required: true},
+          "Speed Defense": {type: Number, required: true},
+        },
+      }, {versionKey: false});
+      pokemonModel = mongoose.model("pokemons", pokemonSchema)
+
+      let pokemons;
+      await axios.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json")
+        .then((res) => {
+          pokemons = res.data
+          pokemons.forEach(poke => {
+            poke.base["Speed Attack"] = poke.base["Sp. Attack"]
+            poke.base["Speed Defense"] = poke.base["Sp. Defense"]
+            delete poke.base["Sp. Attack"];
+            delete poke.base["Sp. Defense"];
+          });
+        })
+        .catch((err) => {
+          console.log('err', err)
+        });
+      
+      await pokemonModel.deleteMany({});
+      await pokemonModel.insertMany(pokemons);
+    } catch (error) {
+      console.log('db connection error', error);
+    }
+    console.log(`Example app listening on port ${port}`)
+  }
+)
+
+// Get batch pokemon data
+app.get("/api/v1/pokemons/", async (req, res) => {
+  let {count, after} = req.query;
+  try {
+    count = parseInt(count)
+    after = parseInt(after)
+    if (!count || !after || isNaN(after) || isNaN(count) || count <= 0 || after <= 0) {
+      return res.status(400).json({errMsg: "Count or After query params might be missing or has an invalid value!", status: "ClientError"})
+    }
+
+    pokemonModel.find({}).skip(after).limit(count)
+    .then((respond) => {
+      if (respond.length === 0) {
+        return res.status(400).json({status: "Error", errMsg: "Invalid query! There are no more pokemons!"})
+      }
+      res.status(200).json({data: respond, status: "Success"})
+    })
+  } catch (err) {
+    return res.status(500).json({errMsg: "Server Error occured"})
+  }  
+})
+
+// Add a pokemon
+app.post("/api/v1/pokemon", async (req, res) => {
+  const pokemonValues = req.body;
+  if (!pokemonValues || Object.keys(pokemonValues).length === 0) {
+    return res.status(400).json({status: "ClientError", errMsg: "request body is missing!"})
+  }
+
+  const {id} = req.body; 
+  if (!id) {
+    return res.status(400).json({status: "ClientError", errMsg: 
+      `Id is missing from the request body`
+  })
+  }
+
+  const pokemon = await pokemonModel.find({id: id})
+  if (pokemon.length > 0) {
+    console.log('pokemon', pokemon)
+    return res.status(400).json({status: "ClientError", errMsg: 
+      `Pokemon with id ${id} already exists`
+    })
+  }
+
+  await pokemonModel.create(pokemonValues)
+    .then((doc) => {
+      return res.status(200).json({status: "Success!", data: doc})
+    })
+    .catch((err) => {
+      console.log('err', err)
+      return res.status(500).json({status: "ServerErrror", errMsg: "Error encountered when creating a pokemon"})
+    })
+})
+
+// Get a pokemon data by id
+app.get("/api/v1/pokemon/:id", async (req, res) => {
+  const {id} = req.params
+  if (!id) {
+    return res.status(400).json({status: "ClientError", errMsg: "pokemonId is missing in the request params!"})
+  }
+
+  await pokemonModel.find({id: id})
+    .then((pokemon) => {
+      if (pokemon.length !== 1) {
+        return res.status(400).json({status: "Error", errMsg: "There are no pokemon with id " + id})
+      }
+      return res.status(200).json({status: "Success", data: pokemon})
+    })
+    .catch((err) => {
+      return res.status(500).json({status: "Error", errMsg: "There is no pokemon with id " + id})
+    })
+})
+
+// Get pokemon image
+app.get("/api/v1/pokemonImage/:id", async (req, res) => {
+  const {id} = req.params
+  if (!id) {
+    return res.status(400).json({status: "ClientError", errMsg: "pokemonId is missing in the request params!"})
+  }
+
+  try {
+    let pokemonAPIId;
+    await pokemonModel.find({_id: id})
+      .then((pokemon) => {
+        pokemonAPIId = pokemon[0].id
+      })
+      .catch(err => {
+        return res.status(400).json({status: "Error", errMsg: "Issue found when getting pokemonId of " + id})
+      })
+
+    const baseImageLinkURL = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/"
+    return res.status(200).json({status: "Success", imageLink: `${baseImageLinkURL}` +`${getThreeCharDigit(pokemonAPIId)}` + `.png`})
+  } catch (err) {
+    console.log('err', err)
+    return res.status(500).json({status: "Error", errMsg: err})
+  }
+})
+
+// Delete a pokemon
+app.delete("/api/v1/pokemon/:id", async (req, res) => {
+  const {id} = req.params
+  if (!id) {
+    return res.status(400).json({status: "ClientError", errMsg: "pokemonId is missing in the request params!"})
+  }
+
+  const pokemon = await pokemonModel.find({id: id})
+  if (pokemon.length == 0) {
+    return res.status(400).json({status: "ClientError", errMsg: 
+      `Pokemon with id ${id} does not exist`
+    })
+  }
+
+  await pokemonModel.deleteOne({id: id})
+    .then(respond => {
+      console.log('doc', respond)
+      return res.status(200).json({status: "Success", msg: `Pokemon with id ${id} has been successfully deleted`})
+    })
+    .catch(err => {
+      console.error(`Error found when deleting a pokemon with id ${id}: ${err}`)
+      return res.status(500).json({status: "Error", errMsg: "Issue found when deleting pokemon with id " + id})
+    })
+})
+
+// Upsert a partial pokemon document
+app.patch("/api/v1/pokemon/:id", async (req, res) => {
+  const {id} = req.params
+  const newPokemonValues = req.body
+
+  const pokemon = await pokemonModel.find({id: id})
+  if (pokemon.length == 0) {
+    return res.status(400).json({status: "ClientError", errMsg: 
+      `Pokemon with id ${id} does not exists`
+    })
+  }
+
+  try {
+    await pokemonModel.updateOne({id: id}, newPokemonValues)
+      .then(doc => {
+        return res.status(200).json({status: "Success", data: {newPokemonValues}})
+      })
+  } catch (err) {
+    return res.status(500).json({status: "Errror", errMsg: `Error when upserting pokemon ${id}`})
+  }
+})
+
+// Update the entire pokemon document
+app.put("/api/v1/pokemon/:id", async (req, res) => {
+  const {id} = req.params
+  const newPokemonValues = req.body
+
+  const pokemon = await pokemonModel.find({id: id})
+  if (pokemon.length == 0) {
+    await pokemonModel.create(newPokemonValues)
+    .then((doc) => {
+      return res.status(200).json({status: "Success!", data: doc})
+    })
+    .catch((err) => {
+      console.log('err', err)
+      return res.status(500).json({status: "ServerErrror", errMsg: "Error encountered when creating a pokemon"})
+    })
+  } else {
+    // if a pokemon already exist, replace the values instead
+    try {
+      await pokemonModel.findOneAndUpdate({id: id}, newPokemonValues, {upsert: true})
+        .then(doc => {
+          return res.status(200).json({status: "Success", data: newPokemonValues})
+        })
+        .catch(err => {
+          return res.status(400).json({status: "ClientError", errMsg: "One or more properties may be invalid"})
+        })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({status: "Errror", errMsg: `Error when updating pokemon ${id}`})
+    }
+  }
+})
+
+// Lecture week 6 - Advance d Filtering
+app.get("/pokemonAdvancedFileteringOld", async (req, res) => {  
+  try {
+    const {page, pagePerHit, sort, filteredProperty, ...query} = req.query
+
+    let pokemonQuery = pokemonModel.find(query).skip(page ?? 0).limit(pagePerHit ?? 5)
+
+    if (sort) {
+      pokemonQuery = pokemonQuery.sort(sort)
+    }
+    if (filteredProperty) {
+      let filterProps = filteredProperty.replaceAll(",", " ")
+      pokemonQuery = pokemonQuery.select(filterProps)
+    }
+    const pokemons = await pokemonQuery
+
+    res.status(200).json({status: "Success", data: pokemons})
+  } catch (e) {
+    console.error("Error when filtering advanced pokemon" + e)
+    res.status(500).json({status: "ServerError", errMsg: e})
+  }
+})
+
+// Tues Midterm practice
+app.get("/pokemonsAdvancedFiltering", async (req, res) => {
+  const {comparisonOperators} = req.query
+  const stats = ["HP", "Attack", "Defense", "Speed", "Speed Attack", "Speed Defense"]
+  let comparisonItems = comparisonOperators.split(",")
+
+  comparisonItems = comparisonItems.map(comparison => {
+    return comparison = comparison.trim()
+  })
+
+  const pokemon = await pokemonModel.find({"base.HP": {$lte: 1}})
+  console.log('pokemon', pokemon)
+
+  // let queryBuilder = pokemonModel.find()
+  
+  // comparisonItems.forEach(item => {
+  //   if (item.match(/\w*<=\d*/g)) {
+  //     console.log("im less than or equal to")
+  //     const operatorValue = item.split("<=")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).lte(parseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else if (item.match(/\w*<\d*/g)) {
+  //     console.log("im less than ")
+  //     const operatorValue = item.split("<")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).lt(parsseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else if (item.match(/\w*>=\d*/g)) {
+  //     console.log("im greater than or equal to ")
+  //     const operatorValue = item.split(">=")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).gte(parseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else if (item.match(/\w*>\d*/g)) {
+  //     console.log("im greater than ")
+  //     const operatorValue = item.split(">")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).gt(parseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else if (item.match(/w*==\d*/g)) {
+  //     console.log("im equal to ")
+  //     const operatorValue = item.split("==")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).equals(parseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else if (item.match(/w*!=\d*/g)) {
+  //     console.log("im not equal to ")
+  //     const operatorValue = item.split("!=")
+  //     queryBuilder = queryBuilder.where(`base.${operatorValue[0]}`).ne(parseInt(operatorValue[1]))
+  //     console.log('operatorValue', operatorValue)
+  //   } else {
+  //     console.log("im invalid ")
+  //   }
+  // })
+
+  // const queryRes = await queryBuilder
+
+  // res.json(queryRes)
+  res.end()
+})
+
+// Tuesday second midterm practice
+app.patch("/pokemonsAdvancedUpdate", async (req, res) => {
+  let {id, pushOperator} = req.query
+  try {
+    const pokemon = await pokemonModel.find({id: id})
+    if (pokemon.length == 0) {
+      res.status(400).json({status: 'ClientError', errMsg: `No pokemon found with id ${id}`})
+    }
+
+    const pokemonTypes = pokemon[0].type
+    pushOperator = pushOperator.trim()
+    pushOperator = pushOperator.substring(1, pushOperator.length - 1)
+    pushItems = pushOperator.split(",")
+    const newPokemonTypes = [...pokemonTypes, ...pushItems]
+    
+    await pokemonModel.updateOne({id: id}, {type: newPokemonTypes})
+    res.status(200).json({status: "Success", data: newPokemonTypes})
+  } catch (err) {
+    console.error("Error when updating a pokemon" + err)
+    res.status(500).json({status: "ServerError", errMsg: "Error when updating a pokemon"})
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send({
+  status: 404,
+  error: "Not Found"
+  })
+ })
+
+
+
